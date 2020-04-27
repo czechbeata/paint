66b32f303c703f89ae2ee0bd260fd736a99ede81
Beata <bea-web.outlook.com>
2020-04-27 23:39:55 +0200

    Added dot draw function

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const StyledCanvas = styled.canvas`
-
+  display:block;
   background:peachpuff;
 `;
 
@@ -18,8 +18,8 @@ class Canvas extends Component {
     }
 
     componentDidMount() {
-        this.canvas.height = window.innerHeight;
-        this.canvas.width = window.innerWidth;
+        // this.canvas.height = window.innerHeight;
+        // this.canvas.width = window.innerWidth;
         this.ctx = this.canvas.getContext('2d');
         this.ctx.lineWidth = 5;
         this.ctx.lineCap = "round";
@@ -38,33 +38,48 @@ class Canvas extends Component {
         this.canvas.addEventListener("mouseup", this.finishPainting);
         this.canvas.addEventListener("mousemove", this.paint);
     }
-    startPainting = () => {
+    startPainting = (e) => {
         this.setState({
             painting: true
+        }, () => {
+            this.paint(e);
         })
     }
     finishPainting = () => {
         this.setState({
             painting: false
         })
-        this.ctx.beginPath();
     }
+
+    getPosition (e) {
+        var rect = this.canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        return [x, y];
+    }
+        
     paint (e) {
-        if (this.state.painting) {
-            this.ctx.lineTo(e.clientX, e.clientY);
-            this.ctx.stroke();
+        if(this.state.painting){
+            var x,y;
+            [x,y] = this.getPosition(e);
             this.ctx.beginPath();
-            this.ctx.moveTo(e.clientX, e.clientY);
-   
-        } else return;      
+            this.ctx.arc(x,y, 5, 0, Math.PI*2);
+            this.ctx.fill();
+
+            
+
+        }   
     }
+
+    
+
     render() { 
         return ( 
             <div>
                 <StyledCanvas  ref={(ref) => (this.canvas = ref)}></StyledCanvas>
-                <button onClick={this.onChangeWidth}>change</button>
+                {/* <button onClick={this.onChangeWidth}>change</button> */}
                 <button onClick={this.paintingOn}>paint</button>
-                <button onClick={this.onChangeColor}>yellow</button>
+                {/* <button onClick={this.onChangeColor}>yellow</button> */}
             </div>
          );
     }
